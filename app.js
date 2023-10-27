1bcb5ebd31de2b92458bca55fcf3d5e562e19385
faquinoortiz <144743802+faquinoortiz@users.noreply.github.com>
2023-10-27 11:13:44 -0700

    error handlers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const data = require('./data.json'); 
 const path = require('path');
 
-// Set your "view engine" to "pug"
+// Setting view engine to pug
 app.set('view engine', 'pug');
 
 // Use a static route to serve static files located in the 'public' folder
@@ -12,14 +12,13 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // Setting routes
 app.get('/', (req, res) => {
- 
-  res.render('index', { projects: data.projects });
+res.render('index', { projects: data.projects });
 });
 
 app.get('/about', (req, res) => {
-  
   res.render('about');
 });
+
 
 app.get('/project/:id', (req, res) => {
   const projectId = parseInt(req.params.id);
@@ -35,8 +34,25 @@ app.get('/project/:id', (req, res) => {
   }
 });
 
+// 404 handler for undefined routes
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    console.error(`Error: ${err.message}, Status: ${err.status}`);
+    res.status(404).render('not-found', { error: err });
+  });
+  
+// Global error handler
+  app.use((err, req, res, next) => {
+    err.status = err.status || 500;
+    err.message = err.message || 'Server Error';
+    console.error(`Error: ${err.message}, Status: ${err.status}`);
+    res.status(err.status).render('error', { error: err });
+  });
+
+
 // Start the server and listen on port 3000
 const port = 3000;
 app.listen(port, () => {
-  console.log(`The application is running on port ${port}`);
+  console.log("The application is running on localhost:3000");
 });
